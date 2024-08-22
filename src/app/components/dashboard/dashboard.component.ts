fd473305109cc0802951bc066787802bb455bf86
Hardik Jain <Hardik.Jain@godigit.com>
2024-08-23 03:51:38 +0530

    feat/search

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,17 +7,31 @@ import { HttpService } from 'src/app/services/http.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  cards: any;
-  constructor(private http: HttpService) { }
+  cards: any[] = [];
+  filteredCards: any[] = [];
+  searchTerm: string = '';
+
+  constructor(private http: HttpService) {}
+
   dashboardData?: string;
+
   ngOnInit(): void {
     this.http.getApiCall("").subscribe({
       next: (res: any) => {
         this.cards = res;
+        this.filteredCards = res;
       },
       error: (err: Error) => {
         console.log(err);
       }
     });
+  }
+
+  filterCards(): void {
+    this.filteredCards = this.cards.filter(card => 
+      card.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      card.email.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      card.address.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
